c7feed9ee3fde82b3d3a31b6c06953a7fc11c627
pumba30 <pumba303030@yandex.ru>
2016-07-31 16:13:03 +0300

    add popup keboard, request focus

--- app/src/main/java/com/pumba30/soundcloudplayer/ui/activity/SearchActivity.java
+++ app/src/main/java/com/pumba30/soundcloudplayer/ui/activity/SearchActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.pumba30.soundcloudplayer.R;
 import com.pumba30.soundcloudplayer.utils.Utils;
@@ -27,13 +30,24 @@ public class SearchActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_search);
 
-
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search_activity, menu);
+
+        MenuItem itemSearch = menu.findItem(R.id.menu_search);
+        final SearchView searchView = (SearchView) itemSearch.getActionView();
+        searchView.setIconifiedByDefault(false);
+        searchView.requestFocus();
 
         return true;
     }
